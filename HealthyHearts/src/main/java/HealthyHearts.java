167b887a5cf54ad41adea840c0b5e9e14c1b5e4d
Ignotas Burba <ignasneprisimenu@gmail.com>
2021-10-01 12:50:47 +0100

    Added comments for HealthyHearts project

--- HealthyHearts/src/main/java/HealthyHearts.java
+++ HealthyHearts/src/main/java/HealthyHearts.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
 public class HealthyHearts {
+
+    /**Static method which runs a simple calculator for maximum and recommended beats per minute
+    *   After inputting their age in terminal information about beats per minute will be presented
+    */
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int maxbpm;
+        //Print out the software name and ask what is their age.
         System.out.println("--- exec-maven-plugin:1.2.1:exec (default-cli) @ Unit1 ---\nWhat is your age?");
-        maxbpm = 220 - reader.nextInt();
+        //The maximum heart rate should be 220 - their age.
+        int maxBpm = 220 - reader.nextInt();
+        //The target heart rate zone is the 50 - 85% of the maximum.
         System.out.println(
-                "Your maximum heart rate should be " + maxbpm + " beats per minute\n" +
-                "Your target HR Zone is " + String.format("%.0f", (maxbpm * 0.5)) + " - " + String.format("%.0f", (maxbpm * 0.85)) + " beats per minute");
+                "Your maximum heart rate should be " + maxBpm + " beats per minute\n" +
+                "Your target HR Zone is " + String.format("%.0f", (maxBpm * 0.5)) + " - " + String.format("%.0f", (maxBpm * 0.85)) + " beats per minute");
     }
 }
